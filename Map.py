7659f90dc415bef69babef426847a7b0533051b3
allie1587 <130801516+allie1587@users.noreply.github.com>
2024-09-09 10:26:22 -0500

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,1 +1,20 @@
-
+'''
+Authors: Alexandra, Sophia, Eli, Jose, and Riley
+Date: 09/08/2024
+Last modified: 09/09/2024
+Purpose: 
+'''
+from Ship import Ships
+class Map:
+    def __init__(self):
+        #self.map = list of list
+        self.ships = []
+    def add_ship(self,length,start,direction):
+        ship = Ships(length)
+        #Check if space is free
+    def update_map(self,row,col):
+        #Checks if it was a hit/miss/sunk and updates accordinally
+        pass
+    def print_map(self):
+        #displays the map
+        pass
