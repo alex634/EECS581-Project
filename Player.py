93be629daa085b244980549410fabfb7407dbe27
allie1587 <130801516+allie1587@users.noreply.github.com>
2024-09-09 10:36:48 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,1 +1,24 @@
-
+'''
+Authors: Alexandra, Sophia, Eli, Jose, and Riley
+Date: 09/08/2024
+Last modified: 09/09/2024
+Purpose: 
+'''
+from Map import Map
+class Player:
+    def __init__(self,name,num_ships):
+        self.playerMap = Map()
+        self.opponentMap = Map()
+    def displayMaps(self):
+        #display both boards
+        self.playerMap.display()
+        self.opponentMap.display()
+    def addShip(self,length,direction,start):
+        self.playerMap.add_ship(length,direction,start)
+        self.playerMap.display()
+    def updateOpponent(self,row,col):
+        #Update the map 
+        pass
+    def updatePlayer(self,row,col):
+        #Update the map
+        pass
