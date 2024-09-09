0ec795701bd4d4c8fa3fdeebe0e7c22242d43b7b
allie1587 <130801516+allie1587@users.noreply.github.com>
2024-09-09 11:45:29 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,4 +5,20 @@ Last modified: 09/09/2024
 Purpose: 
 '''
 from Player import Player
-
+def clear():
+    #ask player to enter
+    #clear terminal
+    #ask player to pass to next player
+    #ask next player to press enter
+    #return 
+    #where you return will then need to print the next player's boards
+    pass
+def placeShips(player,numShips):
+    #make the player place ships
+    #start with the highest number first
+    pass
+def turn(player1, player2):
+    #use a while loop to 
+    pass
+def main():
+    pass
