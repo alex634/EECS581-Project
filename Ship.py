1cfdcd310a58b13a8de9ec2c56b7460725fd75af
allie1587 <130801516+allie1587@users.noreply.github.com>
2024-09-09 10:22:30 -0500

    Update Ship.py

--- Ship.py
+++ Ship.py
@@ -1,1 +1,15 @@
-
+'''
+Authors: Alexandra, Sophia, Eli, Jose, and Riley
+Date: 08/20/2024
+Last modified: 08/26/2024
+Purpose: 
+'''
+class Ships:
+    def __init__(self,length):
+        self.hits = 0
+        self.length = length
+        self.sunk = False
+    def hit(self):
+        self.hits += 1
+        if self.hits == self.length:
+            self.sunk = True
