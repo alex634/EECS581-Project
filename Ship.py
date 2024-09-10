a80eb5b6728a9611053bbfc920d0be6b71b81226
JoseLeyba <123515981+JoseLeyba@users.noreply.github.com>
2024-09-10 08:27:24 -0500

    Update Ship.py

--- Ship.py
+++ Ship.py
@@ -5,10 +5,11 @@ Last modified: 09/09/2024
 Purpose: 
 '''
 class Ships:
-    def __init__(self,length):
+    def __init__(self,length, location):
         self.hits = 0
         self.length = length
         self.sunk = False
+        self.location = location
     def hit(self):
         self.hits += 1
         if self.hits == self.length:
