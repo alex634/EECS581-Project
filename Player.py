7782daf3d407bfd8ce6a7f36b8c7aece657d452c
Sophia Medallada <154625869+sophia-medallada@users.noreply.github.com>
2024-09-14 15:11:50 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -2,40 +2,38 @@
 Authors: Alexandra, Sophia, Eli, Jose, and Riley
 Date: 09/08/2024
 Last modified: 09/14/2024
-Purpose: Class for a player
+Purpose: 
 '''
 from Map import Map
 class Player:
-    def __init__(self):
-        self.playerMap = Map()
-        self.opponentMap = Map()
-        self.fleetNum = 0
-        self.opponentSunk = 0
-        
-    def displayMaps(self):
-        #display both boards
-        print('Your Board')
-        self.playerMap.display()
-        print('Opponents Board')
-        self.opponentMap.display()
-    def displayEmpty(self):
-        self.playerMap.display()
-
-    def addToFleet(self,length,row, col,direction):
-        self.playerMap.placeShip(length,row,col,direction)
-        self.fleetNum += 1
-        self.opponentSunk += 1
-        self.playerMap.display()
-
-    def updateOpponent(self,row,col,opponent):
-        #Update the map 
-        result = self.opponentMap.updateOpponentMap(row,col,opponent)
-        #self.opponentMap.display()
-        if result == 2:
-            self.opponentSunk -= 1
-        return result
+    def __init__(self):                      #creates a class for the player and opponent board
+        self.playerMap = Map()                              #player map
+        self.opponentMap = Map()                            #opponent map
+        self.fleetNum = 0                                   #fleet number set to 0 for the counter
+        self.opponentSunk = 0                               #opponent sunk set to 0 for counter
     
-    def updatePlayer(self,row,col,opponent):
-        #Update the map
-        return self.playerMap.updatePlayerMap(row,col,opponent)
+    def displayMaps(self):                                  #function to display the boards for the player and opponent board
+        print("Your Board")                                 #prints your board on the screen
+        self.playerMap.display()                            #display of the player map
+        print("Opponents Board")                            #prints opponents board on the screen
+        self.opponentMap.display()                          #display of the opponent map
+    
+    def displayEmpty(self):                                 #function to display the empty map once it's the player's turn 
+        self.playerMap.display()                            #display player's map  
+   
+    def addToFleet(self,length,row,col, direction):          #function to add the ships to the fleet 
+        self.playerMap.placeShip(length,row,col,direction)  #places the ships to the player map with the length of ship, row, column and direction
+        self.opponentSunk += 1                              #counter for when the opponent's ship is sunk
+        self.fleetNum += 1                                  #counts the fleet after adding the ship
+        self.playerMap.display()                            #displays the player map of ship after adding to the fleet
+
+    def updateOpponent(self,row,col,opponent):                       #updates the map of the opponent
+        result = self.opponentMap.updateOpponentMap(row,col,opponent)#result variable that will update the opponent's row and column
+        #self.opponentMap.display()                         #this will then display the map after updating the opponent's row and column
+        if result == 2:                                     #if the result is 2 
+            self.opponentSunk -= 1                          #decreases the opponent's sunk counter
+        return result                                       #returns the result of the updated map
+    
+    def updatePlayer(self,row,col,opponent):                         #function to update the player's board 
+        self.playerMap.updatePlayerMap(row,col,opponent)             #calls the player's map and updates the row and column
     
