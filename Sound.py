86e3df360506693a4086a751754c82874bef14ba
Timo Alejandro Aranjo <alex.aranjo@gmail.com>
2024-09-27 14:17:41 -0500

    Partially adding sound system

---
+++ Sound.py
@@ -0,0 +1,21 @@
+from playsound import playsound
+
+class Sound:
+    @staticmethod
+    def play_Missed():
+        playsound('sounds/splash_cc0.wav')
+    @staticmethod
+    def play_Win():
+        playsound('sounds/win_cc0.wav')
+    @staticmethod
+    def play_Error():
+        playsound('sounds/error_cc0.wav')
+    @staticmethod
+    def play_Hit():
+        playsound('sounds/hit_cc0.wav')
+    @staticmethod
+    def play_Sunk():
+        playsound('sounds/cloud_cc0.wav')
+    @staticmethod
+    def play_Turn():
+        playsound('sounds/turn_cc0.wav')
